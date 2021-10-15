a856231bf16ab83eeefede9e84d80c0fd38454fe
Markus Sträßer <m.straesser@ymail.com>
2021-10-15 13:12:08 +0200

    changed plotwidth to 1 because of performance

--- PlotCSV.py
+++ PlotCSV.py
@@ -4,7 +4,7 @@ import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
 
-f = "Sensordata\RawData13102021.csv"
+f = "Sensordata\RawData15102021.csv"
 
 # get the Data
 data = np.genfromtxt(f, delimiter=",", names=["Timestamp", "Pressure"])
@@ -19,7 +19,7 @@ win.setWindowTitle("Dashboard")
 # create the plot object
 plot1 = win.addPlot(title="Raw Ballistocardiography Data")
 # plot the data
-plot1.plot(data["Pressure"], pen=pg.mkPen((0, 153, 255), width=2))
+plot1.plot(data["Pressure"], pen=pg.mkPen(0, 153, 255))
 
 # create the GUI-instance of QApplication
 if __name__ == "__main__":
