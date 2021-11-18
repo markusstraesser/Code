f5c8b1a888a33c339a8ea533ad10b0692c106056
Markus Sträßer <m.straesser@ymail.com>
2021-11-18 19:25:51 +0100

    HR + HRV Plots complete

--- SleepAnalysisDSP.py
+++ SleepAnalysisDSP.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.signal import butter, lfilter
 import heartpy as hp
-from scipy.signal import resample
+from scipy.signal import resample, find_peaks
+import matplotlib.gridspec as gridspec
 
 
 def getSamplingFreq(timestamp):
@@ -15,14 +16,14 @@ def getSamplingFreq(timestamp):
     t_stop = timestamp[-1]  # ts of last sample
     delta_t = t_stop - t_start  # time difference in microseconds
     # number of samples / time in seconds: sampling frequency in Hz
-    fs = len(timestamp) / delta_t * 1000000
+    fs = len(timestamp) / delta_t * 1_000_000
     return fs
 
 
 def getDuration(timestamp):
     t_start = timestamp[0]  # ts of first sample
     t_stop = timestamp[-1]  # ts of last sample
-    delta_t = int((t_stop - t_start) / 1000000)  # time difference in seconds
+    delta_t = int((t_stop - t_start) / 1_000_000)  # time difference in seconds
     return str(timedelta(seconds=delta_t))
 
 
@@ -52,12 +53,12 @@ def filter_hr(smoothed_v: pd.DataFrame, fs):
     )
 
 
-def HeartR(hr: list, fs):
+def HR_HRV(hr: list, fs):
     lower = 0
-    upper = int(fs * 30)
+    upper = int(fs * 60)
     print(f"Sampling Frequency Resampled: {fs:.3f}")
     timer = 0
-    hr_vals, timecodes = [], []
+    hr_vals, hrv_vals, timecodes = [], [], []
     # print(len(ts))
     while upper <= len(hr):
         try:
@@ -66,16 +67,17 @@ def HeartR(hr: list, fs):
                 fs,
                 clean_rr=True,
             )
-            hp.process
             hr_vals.append(m["bpm"])
+            hrv_vals.append(m["rmssd"])
             timecodes.append(timer)
         except:
-            timecodes.append(timer)
             hr_vals.append(0)
+            hrv_vals.append(0)
+            timecodes.append(timer)
         lower = upper
-        upper += int(fs * 30)
-        timer += 0.5
-    return hr_vals, timecodes
+        upper += int(fs * 60)
+        timer += 1
+    return hr_vals, hrv_vals, timecodes
 
 
 def filter_rr(smoothed_v: pd.DataFrame, fs):
@@ -90,11 +92,30 @@ def filter_rr(smoothed_v: pd.DataFrame, fs):
     )
 
 
-def RespR(data):
-    # TODO write respiratory rate calculation
-    rr_vals = np.empty(dtype=int)
-    # go through the data and calculate RR for every Minute
-    return rr_vals
+def RespR(rr: list, fs):
+    lower = 0
+    upper = int(fs * 30)
+    print(f"Sampling Frequency Resampled: {fs:.3f}")
+    timer = 0
+    rr_vals, timecodes = [], []
+    # print(len(ts))
+    while upper <= len(hr):
+        try:
+            wd, m = hp.process(
+                rr[lower:upper],
+                fs,
+                clean_rr=True,
+            )
+            hp.process
+            hr_vals.append(m["bpm"])
+            timecodes.append(timer)
+        except:
+            timecodes.append(timer)
+            hr_vals.append(0)
+        lower = upper
+        upper += int(fs * 30)
+        timer += 0.5
+    return hr_vals, timecodes
 
 
 def MvtR(data):
@@ -122,78 +143,50 @@ def plot_dash(
     rrdata_filt,
     hr_sections,
     hrv_sections,
-    rr_sections,
-    mvt_sections,
-    sleepphases,
+    # rr_sections,
+    # mvt_sections,
+    section_timestamps
+    # sleep_phases,
 ):
     """Creates Plots for:
     - Complete Raw Dataset
-    - Sections of Filtered Heartrate and Respiratory Signal
-    - Complete Dataset of Heartrate, HR-Variability, Respiratory Rate and Movement in 30 Second Sections
-    - Complete Sleep Phases"""
-    fig, ax = plt.subplots(3, 1)
-    fig.set_size_inches(16, 9)
-    tot_duration = getDuration(data["smoothed_ts"])
-    sec_duration = getDuration(data["smoothed_ts"].iloc[lower_b:upper_b])
-    plt.suptitle(
-        f"BCG Sensor Data\nTotal Time: {tot_duration}, Length of Section: {sec_duration}"
-    )
-    for i in range(3):
-        ax[i].set_xlabel("Time (µs)")
-        ax[i].set_ylabel("Sensor Value")
-        ax[i].ticklabel_format(scilimits=(6, 6), useMathText=True)
+    - Zoomed Sections of Filtered Heartrate and Respiratory Signal
+    - Heartrate, HR-Variability, Respiratory Rate and Movement in 60 Second Sections
+    - Sleep Phases"""
 
-    section_width = upper_b - lower_b
-    max_y = data["smoothed_v"].max()
-    xy_arrow = (
-        data["smoothed_ts"].iloc[lower_b],
-        data["smoothed_v"].iloc[lower_b],
-    )
-    xytext = (
-        data["smoothed_ts"].iloc[lower_b] + section_width / 2,
-        max_y * 0.9,
-    )
-    ax[0].annotate(
-        "Section",
-        xy_arrow,
-        xytext,
-        arrowprops=dict(facecolor="black", shrink=0.05),
+    fig = plt.figure(tight_layout=True)
+    fig.set_size_inches(18, 8)
+    figmgr = plt.get_current_fig_manager()
+    figmgr.window.showMaximized()
+    fig.suptitle(
+        f"Sleep Analysis using BCG (Total Recorded Time: {tot_duration})", fontsize=16
     )
 
-    ax[0].plot(
-        data["smoothed_ts"], data["smoothed_v"], label="Smoothed Data", color="blue"
-    )
-    ax[0].legend(loc="upper right")
-    ax[0].text(
-        0,
-        data["smoothed_v"].min(),
-        f"Sampling Frequency complete Data: {fs:.3f} Hertz\nSampling Frequency complete smoothed Data: {fs_sm:.3f} Hertz",
-    )
-    ax[1].plot(
-        data["smoothed_ts"].iloc[lower_b:upper_b],
-        data["smoothed_v"].iloc[lower_b:upper_b],
-        label="Smoothed Data Section",
-        color="blue",
-    )
-    ax[1].legend(loc="upper right")
-    ax[1].text(
-        data["smoothed_ts"].iloc[lower_b:upper_b].min(),
-        data["smoothed_v"].iloc[lower_b:upper_b].min(),
-        f"Sampling Frequency Section: {fs2:.3f} Hertz",
-    )
-    # configure subplot to the selected "param": hr, rr, mvt
-    ax[2].plot(
-        data["smoothed_ts"].iloc[lower_b:upper_b],
-        data[f"filtered_{param}"].iloc[lower_b:upper_b],
-        label=f"Filtered Data Section {param.upper()}",
-        color="orange",
-    )
-    ax[2].legend(loc="upper right")
-    ax[2].text(
-        data["smoothed_ts"].iloc[lower_b:upper_b].min(),
-        data[f"filtered_{param}"].iloc[lower_b:upper_b].min(),
-        f"Sampling Frequency filtered Section: {fs2_sm:.3f} Hertz",
-    )
+    gs = gridspec.GridSpec(nrows=6, ncols=4)
+
+    ax1 = fig.add_subplot(gs[0, :])
+    ax2 = fig.add_subplot(gs[1, :2])
+    ax3 = fig.add_subplot(gs[1, 2:])
+    ax4 = fig.add_subplot(gs[2:4, 0])
+    ax5 = fig.add_subplot(gs[2:4, 1])
+    ax6 = fig.add_subplot(gs[2:4, 2])
+    ax7 = fig.add_subplot(gs[2:4, 3])
+    ax8 = fig.add_subplot(gs[-2:, :])
+
+    ax1.plot(rawdata)
+    ax1.set_title("Raw Sensor Data")
+    ax2.plot(rrdata_filt[250000:252575])
+    ax2.set_title("Filtered Respiratory Signal")
+    ax3.plot(hrdata_filt[250000:252575])
+    ax3.set_title("Filtered Heartrate Signal")
+    ax4.set_title("Respiratory Rate in 1 Minute Intervals")
+    ax5.bar(section_timestamps, hr_sections, width=1)
+    ax5.set_title("Heart Rate in 1 Minute Intervals")
+    ax6.bar(section_timestamps, hrv_sections, width=1)
+    ax6.set_title("Heart Rate Variability in 1 Minute Intervals")
+    ax7.set_title("Movement in 1 Minute Intervals")
+    ax8.set_title("Sleep Phases")
+
     plt.show()
 
 
@@ -210,24 +203,17 @@ if __name__ == "__main__":
     fs_sm = getSamplingFreq(data["smoothed_ts"].to_numpy())
     print(f"Sampling Frequency: {fs_sm:.3f}")
 
-    # filter the smoothed datasets
+    tot_duration = getDuration(data["smoothed_ts"].to_list())
+
+    # filter and upsample the smoothed datasets
     filter_hr(data["smoothed_v"], fs_sm)
     hr_filt = data["filtered_hr"].to_list()
-    res_hr_filt = resample(hr_filt, len(hr_filt) * 4)
+    res_hr_filt = resample(hr_filt, len(hr_filt) * 2)
+    filter_rr(data["smoothed_v"], fs_sm)
+    rr_filt = data["filtered_rr"].to_list()
 
     # Calculate Heartrates, Respiratory Rates, HRV and Movement for 30 Second Intervals
-    heartrates, timecodes = HeartR(res_hr_filt, (fs_sm * 4))
+    heartrates, rmssd, hr_timecodes = HR_HRV(res_hr_filt, (fs_sm * 2))
 
     # Plot everything
-    plt.figure(figsize=(16, 5))
-    plt.subplot(211)
-    plt.title(
-        "Heart Rate in 30 Second Intervals, Total Time: "
-        + getDuration(data["smoothed_ts"].to_list())
-    )
-    plt.xlabel("Time [minutes]")
-    plt.ylabel("Heartrate [bpm]")
-    plt.bar(timecodes, heartrates, width=0.5)
-    plt.subplot(212)
-    plt.plot(hr_filt)
-    plt.show()
+    plot_dash(data["value"], hr_filt, rr_filt, heartrates, rmssd, hr_timecodes)
